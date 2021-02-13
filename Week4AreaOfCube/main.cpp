eb8b56d3654b8c94db0dd42e4e82520faeef30ec
Jackie Ocana <jacquelineocana@my.smccd.edu>
2021-02-13 01:09:35 -0800

    Initial Commit

---
+++ Week4AreaOfCube/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Week4AreaOfCube
+//
+//  Created by Jackie on 2/13/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
